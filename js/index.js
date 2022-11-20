3b0837840988f66e35c6c0bfc0e0016a0c4bcced
Abdullah <abdullah.tahir.skipq@gmail.com>
2022-11-20 19:47:42 +0500

    add theme-switch

--- js/index.js
+++ js/index.js
@@ -1,106 +1,103 @@
-const timeElement =  document.getElementById("time");
-const weekDaysHolder =  document.getElementById("weekDays");
-const weekDays =  ['SUN','MON','TUE','WED','THU','FRI','SAT']
+const timeElement = document.getElementById("time");
+const weekDaysHolder = document.getElementById("weekDays");
+const weekDays = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
 const h = document.getElementById("hour");
 const m = document.getElementById("min");
 const s = document.getElementById("sec");
 const ap = document.getElementById("ap");
 
 function getTime() {
-    let date = new Date();
-    let day = date.getDay();
-    let hour = date.getHours();
-    let mins = date.getMinutes();
-    let sec  = date.getSeconds();
-    let amPm = "AM";
+  let date = new Date();
+  let day = date.getDay();
+  let hour = date.getHours();
+  let mins = date.getMinutes();
+  let sec = date.getSeconds();
+  let amPm = "AM";
 
-    setActiveDay(day);
+  setActiveDay(day);
 
-    if (hour >= 12){
-        amPm = "PM";
-    } else {
-        amPm = "AM";
-    }
+  if (hour >= 12) {
+    amPm = "PM";
+  } else {
+    amPm = "AM";
+  }
 
-    // convert 24 to 12
-    if (hour == 0) {
-        hour = 12;
-    } else if (hour > 12 ) {
-        hour = hour - 12;
-    } else {
-        hour = hour;
-    }
+  // convert 24 to 12
+  if (hour == 0) {
+    hour = 12;
+  } else if (hour > 12) {
+    hour = hour - 12;
+  } else {
+    hour = hour;
+  }
 
-    hour = addZeros(hour);
-    mins = addZeros(mins);
-    sec = addZeros(sec);
-    
-    let timeStr = hour + ":" + mins + ":" + sec + "  " + amPm;
+  hour = addZeros(hour);
+  mins = addZeros(mins);
+  sec = addZeros(sec);
 
-    timeElement.innerText = timeStr;
+  let timeStr = hour + ":" + mins + ":" + sec + "  " + amPm;
 
-    h.innerText=hour;
-    m.innerText=mins;
-    s.innerText=sec;
-    ap.innerText=amPm;
+  timeElement.innerText = timeStr;
 
+  h.innerText = hour;
+  m.innerText = mins;
+  s.innerText = sec;
+  ap.innerText = amPm;
 
-    //console.log("Time : "+weekDays[day])
+  //console.log("Time : "+weekDays[day])
 
+  setTimeout(getTime, 1000);
 
-    setTimeout(getTime,1000);
-
-    return timeStr;
+  return timeStr;
 }
 
-function addZeros(val){
-    let updated ="";
-    if (val < 10) {
-        updated = "0" + val;
+function addZeros(val) {
+  let updated = "";
+  if (val < 10) {
+    updated = "0" + val;
+  } else {
+    updated = val;
+  }
+  return updated;
+}
+
+function setActiveDay(day) {
+  weekDaysHolder.innerHTML = "";
+  let dayInText = weekDays[day];
+
+  for (index in weekDays) {
+    let daySpan = document.createElement("span");
+    daySpan.innerText = weekDays[index] + " ";
+    daySpan.className = "";
+
+    if (day == index) {
+      daySpan.className = "active";
     } else {
-        updated = val;
+      daySpan.className = "in-active";
     }
-    return updated;
-
+    weekDaysHolder.appendChild(daySpan);
+  }
+  return dayInText;
 }
 
-function setActiveDay(day){
-    weekDaysHolder.innerHTML = ""
-    let dayInText = weekDays[day]
-    
-    for( index in weekDays){
-        let daySpan = document.createElement('span');
-        daySpan.innerText = weekDays[index] + " ";
-        daySpan.className="";
+getTime();
+// console.log("main call : "+getTime())
+// console.log("main call  get day: "+setActiveDay(0))
 
-        if(day == index){
-            daySpan.className = "active"
-        }else{
-            daySpan.className = "in-active"
-        }
-        weekDaysHolder.appendChild(daySpan);
-        
-    }
-    return dayInText
+const themeSwitch = document.getElementById("theme-switch");
 
-}
-console.log("main call : "+getTime())
-console.log("main call  get day: "+setActiveDay(0))
+localStorage.setItem("themeMode", "light");
 
-module.exports = { getTime, setActiveDay }
+themeSwitch.addEventListener("click", (e) => {
+  let theme = localStorage.getItem("theme-switch");
 
-/**
- * 
- * 
- * 
- * 
+  if (theme == "light" || theme == "") {
+    document.documentElement.setAttribute("data-themeMode", "dark");
+    localStorage.setItem("themeMode", "dark");
+  } else {
+    document.documentElement.removeAttribute("data-themeMode");
+    localStorage.setItem("themeMode", "light");
+  }
+});
 
-
-
-
-
-
-
-
- */
-// abdullah below
+module.exports = { getTime, setActiveDay };
