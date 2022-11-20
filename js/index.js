fd9cf8a52a00ebe6f71f76c23d6818d901387d0e
Abdullah <abdullah.tahir.skipq@gmail.com>
2022-11-20 17:29:24 +0500

    add theme-switch logic

--- js/index.js
+++ js/index.js
@@ -48,4 +48,18 @@
 
 
  */
-// abdullah below
+const themeSwitch = document.getElementById("theme-switch");
+
+localStorage.setItem("themeMode", "light");
+
+themeSwitch.addEventListener("click", (e) => {
+  let theme = localStorage.getItem("theme-switch");
+
+  if (theme == "light" || theme == "") {
+    document.documentElement.setAttribute("data-themeMode", "dark");
+    localStorage.setItem("themeMode", "dark");
+  } else {
+    document.documentElement.removeAttribute("data-themeMode");
+    localStorage.setItem("themeMode", "light");
+  }
+});
