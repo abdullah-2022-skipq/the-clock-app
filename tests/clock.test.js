0d1ad84764bb115635e225a662231fba8b52c548
abuzar <abuzar.qasim@gmail.com>
2022-11-27 17:43:13 +0500

    ui manipulation and ( Date Time) seperation

--- tests/clock.test.js
+++ tests/clock.test.js
@@ -1,10 +1,17 @@
-const { getTime, setActiveDay } = require("../js/index");
-
+import { getCurrentTime, getCurrentDay } from "../js/dateTime.js";
+const weekDays = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
 function getTimeHelper(string) {
   let hours = string.split(":")[0];
   let mins = string.split(":")[1];
 
   return [hours, mins];
+}
+function getDateHelper() {
+  let date = new Date();
+  let day = date.getDay();
+  let dayInText = weekDays[day];
+
+  return dayInText;
 }
 
 function lenChecker(string) {
@@ -16,7 +23,8 @@ function hourValidator(hours) {
   return hour >= 0 && hour <= 12;
 }
 
-let [hours, mins] = getTimeHelper(getTime());
+let [hours, mins] = getTimeHelper(getCurrentTime());
+let day = getDateHelper();
 
 test("returns if the length of hours is 2", () => {
   expect(lenChecker(hours)).toBe(true);
@@ -29,3 +37,8 @@ test("returns if the length of minutes is 2", () => {
 test("returns if the values of hours is valid", () => {
   expect(hourValidator(hours)).toBe(true);
 });
+
+test("returns True if correct day is returned", () => {
+  expect(getCurrentDay()).toBe(day);
+});
+
